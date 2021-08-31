c3e493a5ecee380ebc6e4967a0d8f56fcc99539d
VISHNUPRIYA776 <82400638+VISHNUPRIYA776@users.noreply.github.com>
2021-08-31 09:48:53 +0530

    Add files via upload

---
+++ Streamlit_Bank_Loan_Prediction-master/run.py
@@ -0,0 +1,15 @@
+import webbrowser
+webbrowser.open('http://streamlit.io')
+
+import platform
+platform.system()
+
+
+st.markdown(
+        """
+        <style>
+        
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
